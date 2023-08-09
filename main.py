475e7c52c79f8d80a68cd921c16d2777be68ea80
Binyamin Y Cohen <binyamincohen555@gmail.com>
2023-08-10 00:14:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,16 @@ from pathlib import Path
 
 from core import info
 from core.object_manager import ObjectManager
-from dotnet import DotNETCompiler, DotNETContext
 from miniz.concrete.module import Module
 from miniz2xml.xml_builder import XMLBuilder
-from zs.zs2miniz.toolchain import Toolchain
 from zs.zs_compiler import ZSCompiler
 
 from zs.cli.options import Options, get_options, InitOptions
-from zs.processing import State
 
-from zs.std.modules.module_core import core
-from zs.std.modules.module_filesystem import filesystem
-from zs.std.modules.module_srf import srf
+from zs.std.importers import ZSImporter, ModuleImporter
+from zs.modules.module_core import module as core
+# from zs.std.modules.module_filesystem import filesystem
+# from zs.std.modules.module_srf import srf
 
 from zs.std.parsers import base as base_language
 
@@ -27,27 +25,32 @@ def main(options: Options):
 
     project_name = Path(options.source).name.split('.')[0].replace('_', ' ').title().replace(' ', "")
 
-    state = State()
+    compiler = ZSCompiler(parser=base_language.get_parser)
+    context = compiler.context
+    state = compiler.state
 
-    parser = base_language.get_parser(state)
+    compiler.toolchain.parser.setup()
 
-    parser.setup()
+    context.import_system.add_directory(Path(options.source).parent)
 
-    compiler = ZSCompiler(toolchain=Toolchain(state=state, parser=parser))
-    context = compiler.toolchain.context
+    context.import_system.add_importer(ZSImporter(compiler.context.import_system), ".zs")
+    context.import_system.add_importer(ModuleImporter(compiler), "module")
 
-    context.add_module_to_cache("core", core)
-    context.add_module_to_cache("srf", srf)
-    context.add_module_to_cache("filesystem", filesystem)
+    context.add_module("core", core)
+    # context.add_module("srf", srf)
+    # context.add_module("filesystem", filesystem)
 
+    # try:
+    #     result = compiler.toolchain.execute_document(options.source, result=ToolchainResult.ResolvedAST)
     try:
         result = compiler.import_document(options.source)
     except Exception as e:
         raise e
     else:
-        module = result.scope.lookup_name(project_name).get()
+        # result
+        module = result.object_scope.lookup_name(project_name, default=None)
         if not isinstance(module, Module):
-            raise TypeError
+            raise TypeError(f"Can't find a module named \'{project_name}\'")
 
         xml_dmp = XMLBuilder(project_name, info.INFO_DICT, ObjectManager())
 
@@ -58,7 +61,7 @@ def main(options: Options):
         state.reset()
 
         for message in state.messages:
-            print(f"[{message.processor.__class__.__name__}] [{message.field_type.value}] {message.origin} -> {message.content}")
+            print(f"[{message.processor.__class__.__name__}] [{message.type.value}]{(' ' + str(message.origin)) if message.origin else ''} -> {message.content}")
 
 
 if __name__ == '__main__':
